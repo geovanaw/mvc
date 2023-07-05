7fdb6fd3036c87dc5d4dfc6a5e92da59075fc578
Geovana Lima <geovana.willyana@gmail.com>
2023-07-05 17:10:51 -0300

    criando rotas

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose';
 
-mongoose.connect();
+mongoose.connect("mongodb+srv://admin:geovana2412@cluster0.rc1qaly.mongodb.net/senac2");
 //A função guarda a string de conexão dentro dos parênteses
 
 let db = mongoose.connection;
 // Agora criamos uma variável que guarda a ação de conectar  nosso usuário com o mongoDB.
 
-module.exports = db;
+export default db;
---
+++ src/routes/api.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import ApiController from '../controllers/apiControllers.js';
+
+const router = express.Router();
+
+router
+.get("/", ApiController.getAllApis)
+
+
